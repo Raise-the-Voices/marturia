bf1b3d02b0695aa2d0234f51d7aa998cee8b2e60
gonzalezlrjesus@gmail.com <gonzalezlrjesus@gmail.com>
2021-01-19 10:38:48 -0400

    fix(header): add a new nav link if the user doesn't have a token valid

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,17 +11,25 @@ const Header = () => {
         <nav className="main-navigation">
           <ul>
             <li>
-              <NavLink to="/" className="header-btn-hover" activeClassName="active">Victims</NavLink>
+              <NavLink exact to="/" className="header-btn-hover" activeClassName="active">Victims</NavLink>
             </li>            
-			{tokenIsStillValid() && 
-			<li>
-              <NavLink to="/submit" className="header-btn-hover" activeClassName="active">Submit</NavLink>
-            </li>
-			}			
-            <li>
-              <NavLink exact to="/admin" className="header-btn-hover" activeClassName="active">Admin</NavLink>
-            </li>
-			<li>
+	
+            {tokenIsStillValid() ? 
+              <>
+                <li>
+                  <NavLink exact to="/submit" className="header-btn-hover" activeClassName="active">Submit</NavLink>
+                </li>
+                <li>
+                  <NavLink exact to="/admin" className="header-btn-hover" activeClassName="active">Admin</NavLink>
+                </li>
+              </>
+            : 
+              <li>
+                <NavLink exact to="/login" className="header-btn-hover" activeClassName="active">Admin</NavLink>
+              </li>
+            }	
+
+			      <li>
               <NavLink exact to="/home" className="header-btn-hover" activeClassName="active">About</NavLink>
             </li>
           </ul>
