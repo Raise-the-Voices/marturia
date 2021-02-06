b8a079bc209b3f8ba6ecd5e678a8b12193f2c630
Jay Moon <sjaymoon15@gmail.com>
2021-02-06 15:51:24 -0600

    refactor(victims): refetch new list instead of reloading the page.

--- src/pages/Victims.js
+++ src/pages/Victims.js
@@ -10,7 +10,6 @@ import data from '../data/countries.json';
 
 const Victims = (props) => {
   const [victimList, setVictimList] = useState(null);
-  const [isSearch, setIsSearch] = useState(false);
   const [name, setName] = useState('');
   const [status, setStatus] = useState('');
   const [country, setCountry] = useState('');
@@ -25,29 +24,37 @@ const Victims = (props) => {
 
   const fetchOptions = async () => {
     try {
-      const response = await axios.get(`${apiUrl}options`);
-      const returnedData = response.data['options-list'].filter(
+      const { data } = await axios.get(`${apiUrl}options`);
+      const filteredOptions = data['options-list'].filter(
         (option) => option.group === 'current_status'
       );
-      setStatuses(returnedData);
+      setStatuses(filteredOptions);
     } catch (error) {
       alert('Something went wrong');
     }
   };
 
-  const fetchVictims = async () => {
+  const fetchVictimsOnLoad = async () => {
     try {
       const query = queryString.parse(props.location.search);
       setCountry(query.country || '');
       setStatus(query.status || '');
       setName(query['victim-name'] || '');
-      let qstr = constructQStr(
+      const qstr = constructQStr(
         query['victim-name'],
         query.country,
         query.status
       );
-      const response = await axios.get(`${apiUrl}victims${qstr}`);
-      const victimList = response.data.victim.map((victim) => {
+      await fetchVictims(qstr);
+    } catch (error) {
+      alert('Something went wrong');
+    }
+  };
+
+  const fetchVictims = async (qstr) => {
+    try {
+      const { data } = await axios.get(`${apiUrl}victims${qstr}`);
+      const victimList = data.victim.map((victim) => {
         return {
           id: victim.ID,
           name: victim.name,
@@ -63,112 +70,103 @@ const Victims = (props) => {
     }
   };
 
+  const handleSubmit = async () => {
+    const qstr = constructQStr(name, country, status);
+    await fetchVictims(qstr);
+  };
+
   useEffect(() => {
     fetchOptions();
-    fetchVictims();
+    fetchVictimsOnLoad();
   }, []);
 
-  let content;
-
-  if (isSearch) {
-    let qstr = constructQStr(name, country, status);
-    content = <Redirect to={'/' + qstr} />;
-    window.location.reload();
-  } else {
-    content = (
-      <MainLayout>
-        <div className='victims page'>
-          <div className='wrapper'>
-            <div className='searchSelect'>
-              <form onSubmit={(e) => e.preventDefault()}>
-                <input
-                  className='search'
-                  placeholder='Search by name...'
-                  onChange={(e) => setName(e.target.value)}
-                  value={name}
-                />
-              </form>
-              <div className='selectSubmit'>
-                <select
-                  id='status'
-                  onChange={(e) => setStatus(e.target.value)}
-                  value={status}
-                >
-                  <option key={'sel'} value='all'>
-                    Select Status
+  return (
+    <MainLayout>
+      <div className='victims page'>
+        <div className='wrapper'>
+          <div className='searchSelect'>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <input
+                className='search'
+                placeholder='Search by name...'
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </form>
+            <div className='selectSubmit'>
+              <select
+                id='status'
+                onChange={(e) => setStatus(e.target.value)}
+                value={status}
+              >
+                <option key={'sel'} value='all'>
+                  Select Status
+                </option>
+                {statuses?.map((item) => (
+                  <option key={item.title} value={item.title}>
+                    {item.title}
                   </option>
-                  {statuses?.map((item) => (
-                    <option key={item.title} value={item.title}>
-                      {item.title}
-                    </option>
-                  ))}
-                </select>
-                <select
-                  id='countries'
-                  onChange={(e) => setCountry(e.target.value)}
-                  value={country}
-                >
-                  <option key={'all'} value='Select Country'>
-                    Select Country
+                ))}
+              </select>
+              <select
+                id='countries'
+                onChange={(e) => setCountry(e.target.value)}
+                value={country}
+              >
+                <option key={'all'} value='Select Country'>
+                  Select Country
+                </option>
+                {data.countries.map((item) => (
+                  <option key={item.country} value={item.country}>
+                    {item.country}
                   </option>
-                  {data.countries.map((item) => (
-                    <option key={item.country} value={item.country}>
-                      {item.country}
-                    </option>
-                  ))}
-                </select>
-                <button
-                  type='submit'
-                  className='btn'
-                  onClick={() => setIsSearch(true)}
-                >
-                  Submit
-                </button>
-              </div>
+                ))}
+              </select>
+              <button type='submit' className='btn' onClick={handleSubmit}>
+                Submit
+              </button>
             </div>
-            <ul className='list'>
-              {victimList && victimList.length !== 0 ? (
-                victimList.map((item, index) => (
-                  <li key={item.id}>
-                    <div className='col'>
-                      {item.url ? (
-                        <img className='photo' src={item.url} alt='victim' />
-                      ) : (
-                        'No photo available'
-                      )}
-                    </div>
-                    <div className='col'>
-                      <div className='name'>
-                        <span>Name:</span> {item.name}
-                      </div>
-                      <div className='dob'>
-                        <span>Date of Birth:</span> {item.dob}
-                      </div>
-                      <div className='location'>
-                        <span>Location: </span> {item.location}
-                      </div>
-                      <div className='status'>
-                        <span>Status: </span> {item.status}
-                      </div>
-                      <div className='more-btn'>
-                        <Link to={'/view/' + String(item.id)}> MORE </Link>
-                      </div>
-                    </div>
-                  </li>
-                ))
-              ) : victimList === null ? (
-                <p> loading... </p>
-              ) : (
-                <p> No victims matching search parameters found </p>
-              )}
-            </ul>
           </div>
+          <ul className='list'>
+            {victimList && victimList.length !== 0 ? (
+              victimList.map((item, index) => (
+                <li key={item.id}>
+                  <div className='col'>
+                    {item.url ? (
+                      <img className='photo' src={item.url} alt='victim' />
+                    ) : (
+                      'No photo available'
+                    )}
+                  </div>
+                  <div className='col'>
+                    <div className='name'>
+                      <span>Name:</span> {item.name}
+                    </div>
+                    <div className='dob'>
+                      <span>Date of Birth:</span> {item.dob}
+                    </div>
+                    <div className='location'>
+                      <span>Location: </span> {item.location}
+                    </div>
+                    <div className='status'>
+                      <span>Status: </span> {item.status}
+                    </div>
+                    <div className='more-btn'>
+                      <Link to={'/view/' + String(item.id)}> MORE </Link>
+                    </div>
+                  </div>
+                </li>
+              ))
+            ) : victimList === null ? (
+              <p> loading... </p>
+            ) : (
+              <p> No victims matching search parameters found </p>
+            )}
+          </ul>
         </div>
-      </MainLayout>
-    );
-  }
-
-  return content;
+      </div>
+    </MainLayout>
+  );
 };
 
 export default Victims;
